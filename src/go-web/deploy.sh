977f695ba93d50d87758aeb27a8fd211d1f549ac
杨活龙 <yanghuolong@zhisland.com>
2017-10-24 11:38:50 +0800

    deploy

---
+++ src/go-web/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+HOST="192.168.2.95"
+USERNAME="yanghuolong"
+PASSWD=""
+PORT="22"
+DIR="~"
+
+echo $USERNAME@$HOST:$DIR
+
+ssh -p $PORT $USERNAME@$HOST "killall go-web" 
+echo "停止旧有项目!"
+
+
+ssh -p $PORT $USERNAME@$HOST "rm -rf $DIR/*" 
+echo "删除旧有目录完成!"
+
+scp -P$PORT go-web $USERNAME@$HOST:$DIR
+scp -p$PORT -r views $USERNAME@$HOST:$DIR
+scp -p$PORT -r static $USERNAME@$HOST:$DIR
+scp -p$PORT -r conf $USERNAME@$HOST:$DIR
+
+echo "部署完成!"
+
+ssh -p $PORT $USERNAME@$HOST "nohup $DIR/go-web > /dev/null &" 
+
+echo "项目启动成功!"
